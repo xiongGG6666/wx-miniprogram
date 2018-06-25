3ad9c82be15919a7bb8c353994147ff54ac765e5
https://github.com/gantao21/thinkJS <a11@HuangHao.local>
2018-06-25 22:43:58 +0800

    1.0.7

--- pages/index/home.js
+++ pages/index/home.js
@@ -4,13 +4,13 @@ const app = getApp()
 Page({
   data: {
     motto: "",
-    phone:"",
+    phone: "",
     userInfo: {},
     hasUserInfo: false,
     canIUse: wx.canIUse('button.open-type.getUserInfo')
   },
   //事件处理函数
-  bindViewTap: function() {
+  bindViewTap: function () {
     wx.navigateTo({
       url: '../logs/logs'
     })
@@ -35,7 +35,7 @@ Page({
         superThis.setData({
           motto: res.data.data.username,
           phone: res.data.data.phone
-        }) ;
+        });
       }
     });
     if (app.globalData.userInfo) {
@@ -44,8 +44,8 @@ Page({
         userInfo: app.globalData.userInfo,
         hasUserInfo: true
       })
-      
-    } else if (this.data.canIUse){
+
+    } else if (this.data.canIUse) {
       // 由于 getUserInfo 是网络请求，可能会在 Page.onLoad 之后才返回
       // 所以此处加入 callback 以防止这种情况
       app.userInfoReadyCallback = res => {
@@ -67,14 +67,22 @@ Page({
         }
       })
     }
-    
+
   },
-  getUserInfo: function(e) {
+  getUserInfo: function (e) {
     console.log(e)
     app.glo443Info = e.detail.userInfo
     this.setData({
       userInfo: e.detail.userInfo,
       hasUserInfo: true
+    })
+  },
+  gotoOrdDtl: function (e) {
+    console.log(e.currentTarget.dataset.type);
+    //// pages/index/orderDetails.js
+
+    wx.navigateTo({
+      url: '../index/order_details?type=' + e.currentTarget.dataset.type
     })
   }
 })
